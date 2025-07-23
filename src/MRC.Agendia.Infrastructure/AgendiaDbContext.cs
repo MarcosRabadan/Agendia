b5b05f0bbce12d68cb8c2758ca87fbb4e68e7fc7
Marcos Rabadán Cobos <marcosrabadanc@outlook.com>
2025-07-23 21:30:39 +0200

    agregados referrencias, inyeccion en program y cadena de conexion

---
+++ src/MRC.Agendia.Infrastructure/AgendiaDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MRC.Agendia.Infrastructure;
+
+public class AgendiaDbContext : DbContext
+{
+    public AgendiaDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+}
